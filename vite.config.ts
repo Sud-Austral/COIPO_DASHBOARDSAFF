265bfa2699f0a36cf96b6e1d82b4a4bd87d115c3
Luis <lmonsalve22@gmail.com>
2026-04-21 13:25:07 -0400

    init

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './', // Important for GitHub Pages
+  base: '/COIPO_DASHBOARDSAFF/', // Updated for GitHub Pages subpath
 });
